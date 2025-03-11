efd163e090f644c09033cfc1ca32b543aa06296b
Gianluca Nieddu <gianluca.nieddu96@gmail.com>
2025-03-11 10:29:40 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,82 @@
+const API_URL = "https://api.pokemontcg.io/v2/cards";
+const cardContainer = document.getElementById("card-container");
+const searchInput = document.getElementById("search");
+const searchButton = document.getElementById("search-btn");
+
+searchButton.addEventListener("click", () => {
+    fetchCards(searchInput.value);
+});
+
+
+
+async function fetchCards(query = "") {
+    let url = API_URL;
+    if (query) {
+        url += `?q=name:${query}`;
+    }
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    displayCards(data.data);
+}
+
+
+function displayCards(cards) {
+    cardContainer.innerHTML = "";
+    cards.forEach(card => {
+        const cardElement = document.createElement("div");
+        cardElement.classList.add("card");
+
+        let price = "N/A";
+        if (card.tcgplayer && card.tcgplayer.prices) {
+            const prices = card.tcgplayer.prices;
+            price =
+                prices.market ??
+                prices.normal?.market ??
+                prices.holofoil?.market ??
+                prices.reverseHolofoil?.market ??
+                "N/A";
+
+            if (price !== "N/A") {
+                price = `$${price.toFixed(2)}`;
+            }
+        }
+
+        cardElement.innerHTML = `
+            <img src="${card.images.small}" alt="${card.name}" class="card-img">
+            <h3>${card.name}</h3>
+            <p>Valore: ${price}</p>
+        `;
+
+        cardContainer.appendChild(cardElement);
+    });
+
+
+    document.querySelectorAll(".card-img").forEach(img => {
+        img.addEventListener("click", function () {
+            document.getElementById("modal-img").src = this.src;
+            document.getElementById("image-modal").style.display = "flex";
+        });
+    });
+
+
+    document.querySelector(".close").addEventListener("click", function () {
+        document.getElementById("image-modal").style.display = "none";
+    });
+
+
+    document.getElementById("image-modal").addEventListener("click", function (event) {
+        if (event.target === this) {
+            this.style.display = "none";
+        }
+    });
+}
+
+
+// searchInput.addEventListener("input", () => {
+//     fetchCards(searchInput.value);
+// });
+
+
+fetchCards();
